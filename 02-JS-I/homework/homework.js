3c8338960789701de93b60fc092273734de671c0
JosemaCeballos <josema.soyhenry@gmail.com>
2022-07-27 22:09:58 -0300

    Finalización TOTAL del homework JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -213,7 +213,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  resultadoEuroParaDolar = valorEuro*1.20;
+  resultadoEuroParaDolar = euro * 1.2;
   return resultadoEuroParaDolar;
 }
 
@@ -224,20 +224,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra = 'a'){
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
     return 'Es vocal';
   }
-  else if (letra = 'e'){
-    return 'Es vocal';
-  }
-  else if (letra = 'i'){
-    return 'Es vocal';
-  }
-  else if (letra = 'o'){
-    return 'Es vocal';
-  }
-  else if (letra = 'u'){
-    return 'Es vocal';
+  else if (letra.length !== 1){
+    return 'Dato Incorrecto';
   }
   else {
     return 'Dato Incorrecto';
