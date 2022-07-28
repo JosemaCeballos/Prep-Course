b1d4d8fe1e10c26e2a6cca2236858faae52997f6
JosemaCeballos <josema.soyhenry@gmail.com>
2022-07-27 21:27:29 -0300

    Finalización del homework JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -72,7 +72,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lengt === str2.length){return true;}
+  if (str1.length === str2.length){return true;}
   else {return false;}
 }
 
@@ -175,8 +175,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  combinNombre = nombre + apellido;
-  return combinNombre;
+  return `${nombre} ${apellido}`;
 }
 
 function obtenerSaludo(nombre) {
@@ -214,6 +213,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  var valorDolar = valorEuro*1.20;
+  return valorDolar;
 }
 
 
@@ -223,7 +224,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra = a, e, i, o, u){
+    return 'Es vocal';
+  }
+  else {
+    return 'Dato Incorrecto';
+  }
 }
 
 
