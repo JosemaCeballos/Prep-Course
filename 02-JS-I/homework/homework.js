3edb359f0e40cef1852b21cafc4f64ea059fcc58
JosemaCeballos <josema.soyhenry@gmail.com>
2022-07-28 01:11:04 -0300

    Final Homework JS-I - last

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,10 +226,10 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length !== 1) {
     return "Dato incorrecto"}
-    else if {( letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
-    return "Es vocal"}
-    else {
-    return "Dato Incorrecto"
+    else if( letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
+      return "Es vocal"}
+    else if (letra !== 'a' || letra !== 'e' || letra !== 'i' || letra !== 'o' || letra !== 'u'){
+      return 'Dato incorrecto'
     }
 }
 
