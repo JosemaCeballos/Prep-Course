5cee482064126760af78992c71e0b383ef1dcabf
JosemaCeballos <josema.soyhenry@gmail.com>
2022-07-30 16:45:26 -0300

    Finalización del Homework JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -115,13 +115,29 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === 0){
+    return 0;
+  }
+  else {
+    multi = 1;
+    for(i=0; i<arguments.length; i++){
+      multi = multi * arguments[i];
+    }
+    return multi;
+  }
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let elementosMayores = [];
+    for (let i = 0; i < arreglo.length; i++){
+    if (arreglo[i] > 18){
+          elementosMayores.push(1);
+   }
+  }
+  return elementosMayores.length;
 }
 
 
@@ -129,7 +145,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
+  //Escribe tu código aquí
+  if (numeroDeDia === 1 || numeroDeDia === 7) {return 'Es fin de semana'}
+  else {return 'Es dia Laboral'}
   
 } 
 
@@ -138,7 +156,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let num;
+  num = n + ' ';
+  if (num[0] === '9') return true;
+  return false;
 }
 
 
@@ -146,7 +167,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (let i=0; i < arreglo.length; i++){
+    if (arreglo[i] !== arreglo[0]){
+      return false;
+    }
+  }
+  return true;
 } 
 
 
@@ -155,6 +181,18 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  arrayTresMeses=[];
+  for (i = 0; i < array.length; i++){
+  if (array[i] === 'Enero' || array[i] === 'Marzo' || array[i] ==='Noviembre'){
+    arrayTresMeses.push(array[i]);
+  }
+}
+  if (arrayTresMeses.length < 3){
+    return 'No se encontraron los meses pedidos'
+  }
+  else {
+    return arrayTresMeses;
+  }
 }
 
 
@@ -162,6 +200,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let nuevoArray = [];
+  for (let i=0; i < array.length; i++){
+    if (array[i] > 100){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
@@ -173,6 +218,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+    let nuevoArray = [];
+    let acum = numero;
+    let aux = 0;
+    for (i=0; i<10; i++){
+      acum = acum + 2;
+      if (aux === acum){
+        break;
+      }
+      nuevoArray.push(acum);
+      aux = aux + 1;
+    }
+    if (aux === acum){
+      return 'Se interrumpió la ejecución';
+    }
+  return nuevoArray;
+
 }
 
 
@@ -183,6 +244,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var acum = numero;
+  var miArray = [];
+  for (var i=1; i<=10 ; i++){
+
+    if (i === 5){continue;}
+
+    acum = acum + 2;
+    miArray.push(acum);
+    
+  }
+  return miArray;
 }
 
 
